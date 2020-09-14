c539deaba188b79ceaca774f0ab02a43a072bd4c
nanangprasetya251015@gmail.com <nanangprasetya251015@gmail.com>
2020-09-15 00:26:33 +0700

    first commit

---
+++ drawer_bloc/lib/Page/root_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RootPage extends StatefulWidget {
+  RootPage({Key key}) : super(key: key);
+
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
